bd623bbad423ef1e729a780decedddb3fe9a85b7
ahmedalnagdy18 <ahmedalnagdy188@gmail.com>
2024-06-13 10:26:39 +0300

    add the right name

--- lib/data/repository/repository_imp.dart
+++ lib/data/repository/repository_imp.dart
@@ -7,8 +7,8 @@ class TaskRepositoryImpl implements TaskRepository {
   @override
   List<Task> addTask(int numberOfTasks, int sequenceOfTasks) {
     for (var i = 0; i < numberOfTasks; i++) {
-      alltasks.add(
-          Task(name: 'Task ${i + 1}', duration: sequenceOfTasks * 60 + i * 60));
+      alltasks.add(Task(
+          name: 'Task ${i + 1}', duration: sequenceOfTasks * 60 + i * 5 * 60));
     }
     return alltasks;
   }
